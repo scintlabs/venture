b11b85c1f71db9b3316f37e6362cca27faf068f6
Tim Kaechle <timothy.kaechle@me.com>
2025-06-06 16:43:43 -0300

    ci.yaml

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,14 +1,14 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1'
-  },
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.jest.json'
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1'
+    },
+    globals: {
+        'ts-jest': {
+            tsconfig: 'tsconfig.jest.json'
+        }
     }
-  }
-};
+}
